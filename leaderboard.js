b7bc409be1af35b4877a650a4badfb3c615a39c9
Mohamed Saleem <mohsal.saleem@gmail.com>
2014-12-01 15:40:20 +0530

    "Upto Color Change"

--- leaderboard.js
+++ leaderboard.js
@@ -5,10 +5,29 @@ if(Meteor.isClient)
             'player': function()
             {
                 return PlayerList.find();
+            },
+            'selectedClass':function(){
+
+                var playerId = this._id;
+                var selectedPlayer = Session.get('selectedPlayer');
+                if(playerId == selectedPlayer)
+                return 'selected';
+
             }
 
 
         });
+    Template.leaderboard.events({
+
+        'click .player':function(){
+
+            var playerId = this._id;
+            Session.set('selectedPlayer',playerId);
+
+    }
+
+
+    });
 }
 
 if(Meteor.isServer)
